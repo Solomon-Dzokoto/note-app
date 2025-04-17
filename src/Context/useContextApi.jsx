861d21b3269b25b4b2bc85ef2427d63ccd236a44
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-04-17 15:00:51 +0000

    Update .gitignore, add redirects file, and refactor context and create note components

--- src/Context/useContextApi.jsx
+++ src/Context/useContextApi.jsx
@@ -1,21 +1,29 @@
-import { useState,createContext,useEffect } from 'react';
- 
+import { useState, createContext, useEffect } from "react";
+
 export const ContextProvider = createContext();
 
-const UseContextApi = ({children}) => {
-  const [notes,setNotes] = useState(JSON.parse(localStorage.getItem("data")) || [])
-  const [search,setSearch]= useState("")
+const UseContextApi = ({ children}) => {
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("data")) || []
+  );
+  const [search, setSearch] = useState("");
 
-  useEffect(()=>{
-    localStorage.setItem("data",JSON.stringify(notes))
-  },[notes])
- const searchNotes = () => notes.filter((note)=>(note.title || note.desc)?.toLowerCase().includes(search.toLocaleLowerCase())
-)
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(notes));
+  }, [notes]);
+  const searchNotes = () =>
+    notes.filter((note) =>
+      (note.title || note.desc)
+        ?.toLowerCase()
+        .includes(search.toLocaleLowerCase())
+    );
   return (
-    <ContextProvider.Provider value={{notes,setNotes,search,setSearch,searchNotes}}>
+    <ContextProvider.Provider
+      value={{ notes, setNotes, search, setSearch, searchNotes }}
+    >
       {children}
     </ContextProvider.Provider>
-  )
-}
+  );
+};
 
-export default UseContextApi
+export default UseContextApi;
--- src/Pages/CreateNote.jsx
+++ src/Pages/CreateNote.jsx
@@ -1,47 +1,50 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useContext } from "react";
 import { ContextProvider } from "../Context/useContextApi";
 import { dateInOrder } from "../assets/Sample";
 import { useNavigate } from "react-router-dom";
 
 const CreateNote = () => {
-    const [title,setTitle] = useState("");
-    const [message,setMessage] = useState("");
-    const {notes,setNotes} = useContext(ContextProvider)
-    const navigate = useNavigate();
-      const date =  dateInOrder();
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const { notes, setNotes } = useContext(ContextProvider);
+  const navigate = useNavigate();
+  const date = dateInOrder();
 
-    const addNote = (e) => {
-       e.preventDefault();
-       const newNote = {id:notes.length + 1,title,desc:message,date }
-       setNotes([newNote,...notes])
-       setMessage("")
-       setTitle("")
-       navigate('/')
-    }
-  
+  const addNote = (e) => {
+    e.preventDefault();
+    const newNote = { id: notes.length + 1, title, desc: message, date };
+    setNotes([newNote, ...notes]);
+    setMessage("");
+    setTitle("");
+    navigate("/");
+  };
+
   return (
     <div>
       <form onSubmit={addNote}>
-        <input 
-        type="text"
-        placeholder="Title..."
-        className="border-o outline-0 form-control"
-        value={title}
-        onChange={(e)=>setTitle(e.target.value)}
-        required
+        <input
+          type="text"
+          placeholder="Title..."
+          className="border-o outline-0 form-control"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
         />
         <textarea
-         cols="53" 
-         placeholder="message..." 
-         className="border-0"
-         value={message}
-         onChange={(e)=>setMessage(e.target.value)}
-         rows="15"></textarea>
-         <button type="submit"  className="btn  btn-primary">Add</button>
+          cols="53"
+          placeholder="message..."
+          className="border-0"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          rows="15"
+        ></textarea>
+        <button type="submit" className="btn  btn-primary">
+          Add
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreateNote
+export default CreateNote;
